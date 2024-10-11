22648afb9a609959f46f6b82c8665c111191c69c
Peter Haberl <peter.haberl@nordcloud.com>
2024-10-11 15:59:10 +0200

    changed dir

---
+++ app/setting.py
@@ -0,0 +1,5 @@
+kafka_setting = {
+    'bootstrap_servers': ["alikafka-post-public-intl-sg-jiy3ydewp02-1-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-jiy3ydewp02-2-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-jiy3ydewp02-3-vpc.alikafka.aliyuncs.com:9092"],
+    'topic_name': 'kafka-vwpoc-topic',
+    'consumer_id': 'Consumer1'
+}
--- setting.py
+++ setting.py
@@ -1,5 +1,5 @@
 kafka_setting = {
-    'bootstrap_servers': ["alikafka-post-public-intl-sg-i5c3yd4gi01-1-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-i5c3yd4gi01-2-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-i5c3yd4gi01-3-vpc.alikafka.aliyuncs.com:9092"],
+    'bootstrap_servers': ["alikafka-post-public-intl-sg-jiy3ydewp02-1-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-jiy3ydewp02-2-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-jiy3ydewp02-3-vpc.alikafka.aliyuncs.com:9092"],
     'topic_name': 'kafka-vwpoc-topic',
     'consumer_id': 'Consumer1'
 }

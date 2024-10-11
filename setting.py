4ebc90331362d8f568cb46c04dcd9acb86b6689a
PeterHaberlNC <118348721+PeterHaberlNC@users.noreply.github.com>
2024-10-11 09:57:37 +0200

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,5 @@
+kafka_setting = {
+    'bootstrap_servers': ["alikafka-post-public-intl-sg-i5c3yd4gi01-1.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-i5c3yd4gi01-2.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-i5c3yd4gi01-3.alikafka.aliyuncs.com:9093"],
+    'topic_name': 'kafka-vwpoc-topic',
+    'consumer_id': 'Consumer1'
+}

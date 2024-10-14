175a63d79af974ba804da5a838a331882786f0e3
PeterHaberlNC <118348721+PeterHaberlNC@users.noreply.github.com>
2024-10-14 18:11:10 +0200

    Update aliyun_kafka_consumer.py

--- aliyun_kafka_consumer.py
+++ aliyun_kafka_consumer.py
@@ -7,6 +7,8 @@ from kafka import KafkaConsumer
 from kafka.errors import KafkaError
 import setting
 
+sVer = "v1.0.0.1 "
+
 conf = setting.kafka_setting
 
 consumer = KafkaConsumer(bootstrap_servers=conf['bootstrap_servers'],
@@ -16,7 +18,7 @@ consumer = KafkaConsumer(bootstrap_servers=conf['bootstrap_servers'],
                         max_poll_records=100,
                         fetch_max_bytes=1 * 1024 * 1024)
 
-print ('consumer start to consuming...')
+print (sVer + 'consumer start to consuming...')
 consumer.subscribe((conf['topic_name'], ))
 for message in consumer:
     print (message.topic, message.offset, message.key, message.value, message.partition)

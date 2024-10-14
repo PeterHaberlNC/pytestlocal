7e36545d82d984f39ededff069faed91f4e57057
PeterHaberlNC <peter.haberl@nordcloud.com>
2024-10-14 17:27:43 +0200

    app folder cleanup

--- aliyun_kafka_consumer.py
+++ aliyun_kafka_consumer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
+# source copied over to /app
 
 import socket
 from kafka import KafkaConsumer
--- app/setting.py
+++
@@ -1,5 +0,0 @@
-kafka_setting = {
-    'bootstrap_servers': ["alikafka-post-public-intl-sg-hjb3yitj109-1-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-hjb3yitj109-2-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-hjb3yitj109-3-vpc.alikafka.aliyuncs.com:9092"],
-    'topic_name': 'kafka-vwpoc-topic',
-    'consumer_id': 'Consumer1'
-}

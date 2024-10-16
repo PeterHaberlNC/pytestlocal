d331acd2197c5ef0c8516f3dde0dee09bc64bc6c
PeterHaberlNC <118348721+PeterHaberlNC@users.noreply.github.com>
2024-10-16 10:27:56 +0200

    Update setting.py after build 24/10/16

--- setting.py
+++ setting.py
@@ -1,5 +1,5 @@
 kafka_setting = {
-    'bootstrap_servers': ["alikafka-post-public-intl-sg-hjb3yitj109-1-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-hjb3yitj109-2-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-hjb3yitj109-3-vpc.alikafka.aliyuncs.com:9092"],
+    'bootstrap_servers': ["alikafka-post-public-intl-sg-b0q3ymkwe01-1.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-b0q3ymkwe01-2.alikafka.aliyuncs.com:9093","alikafka-post-public-intl-sg-b0q3ymkwe01-3.alikafka.aliyuncs.com:9093"],
     'topic_name': 'kafka-vwpoc-topic',
     'consumer_id': 'Consumer1'
 }

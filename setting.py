c8654a0cffe31909b0aae5063d92adcfcedbddb3
PeterHaberlNC <peter.haberl@nordcloud.com>
2024-10-16 13:07:10 +0200

    changed to env variables

--- setting.py
+++ setting.py
@@ -1,5 +1,14 @@
+import os
+bootstrap_servers = os.getenv('bootstrap_servers', '')
+print(f"servers: {bootstrap_servers}")
+topic_name = os.getenv('topic_name', '')
+print(f"topic_name: {topic_name}")
+consumer_group = os.getenv('consumer_group', '')
+print(f"consumer_group: {consumer_group}")
+
+
 kafka_setting = {
-    'bootstrap_servers': ["alikafka-post-public-intl-sg-hjb3yitj109-1-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-hjb3yitj109-2-vpc.alikafka.aliyuncs.com:9092","alikafka-post-public-intl-sg-hjb3yitj109-3-vpc.alikafka.aliyuncs.com:9092"],
-    'topic_name': 'kafka-vwpoc-topic',
-    'consumer_id': 'Consumer1'
+    'bootstrap_servers': [bootstrap_servers],
+    'topic_name': topic_name,
+    'consumer_id': consumer_group
 }

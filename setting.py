949c647700148ca43a61eb14ec889409889273a8
PeterHaberlNC <peter.haberl@nordcloud.com>
2024-10-16 13:43:09 +0200

    bootstrap servers array handling

--- setting.py
+++ setting.py
@@ -1,5 +1,6 @@
 import os
-bootstrap_servers = os.getenv('bootstrap_servers', '')
+bootstrap_servers_s =  os.getenv('bootstrap_servers', '')
+bootstrap_servers = bootstrap_servers_s.split(',')
 print(f"servers: {bootstrap_servers}")
 topic_name = os.getenv('topic_name', '')
 print(f"topic_name: {topic_name}")
@@ -8,7 +9,7 @@ print(f"consumer_group: {consumer_group}")
 
 
 kafka_setting = {
-    'bootstrap_servers': [bootstrap_servers],
+    'bootstrap_servers': bootstrap_servers,
     'topic_name': topic_name,
     'consumer_id': consumer_group
 }
